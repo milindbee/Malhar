7ea691523090e20362d92c7a2e4b75671338e5cf
Milind Barve <milind@datatorrent.com>
2013-11-25 17:14:11 +0530

    Added comments. Also added support for unifier. #301

--- library/src/main/java/com/datatorrent/lib/r/RMax.java
+++ library/src/main/java/com/datatorrent/lib/r/RMax.java
@@ -3,6 +3,7 @@ package com.datatorrent.lib.r;
 import com.datatorrent.api.Context;
 import com.datatorrent.api.DefaultInputPort;
 import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.Operator.Unifier;
 import com.datatorrent.api.annotation.InputPortFieldAnnotation;
 import com.datatorrent.api.annotation.OutputPortFieldAnnotation;
 import com.datatorrent.lib.util.BaseNumberValueOperator;
@@ -14,7 +15,22 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RMax<V extends Number> extends BaseNumberValueOperator {
+/**
+ * Emits at end of window, a maximum of all values sub-classed from Number in the incoming stream. <br>
+ * <br>
+ * <b>StateFull :</b>Yes, max value is computed over application windows. <br>
+ * <b>Partitions :</b>Yes, operator is kin unifier operator. <br>
+ * <br>
+ * <b>Ports</b>:<br>
+ * <b>data</b>: expects V extends Number<br>
+ * <b>max</b>: emits V extends Number<br>
+ * <br>
+ * <br>
+ **/
+
+
+// public class RMax<V extends Number> extends BaseNumberValueOperator {
+public class RMax<V extends Number> extends BaseNumberValueOperator<Number> implements Unifier<Number>{
     private List<Number> vector = new ArrayList<Number>();
     private Rengine rengine;
 
@@ -29,13 +45,31 @@ public class RMax<V extends Number> extends BaseNumberValueOperator {
         @Override
         public void process(Number tuple)
         {
-            vector.add(tuple);
+            RMax.this.process(tuple);
+        }
+    };
+    /**
+     * Adds the received tuple to the vector
+     */
+
+    @Override
+    public void process(Number tuple)
+    {
+        vector.add(tuple);
+    }
+
+    @OutputPortFieldAnnotation(name = "max")
+    public final transient DefaultOutputPort<Number> max = new DefaultOutputPort<Number>()
+    {
+        @Override
+        public Unifier<Number> getUnifier()
+        {
+            return RMax.this;
         }
     };
 
-    @OutputPortFieldAnnotation(name = "min")
-    public final transient DefaultOutputPort<Number> max = new DefaultOutputPort<Number>();
-
+   /* Initialize the R engine
+   */
     @Override
     public void setup(Context.OperatorContext context) {
         super.setup(context);
@@ -49,6 +83,11 @@ public class RMax<V extends Number> extends BaseNumberValueOperator {
         }
 
     }
+
+    /**
+     * Emits a maximum from the values received in the application window.
+     * Clears the vector at the end of the function.
+     **/
 
     @Override
     public void endWindow() {
