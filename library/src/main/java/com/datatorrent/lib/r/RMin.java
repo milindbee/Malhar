85aa181f26aa2ac8fc9af8b84122bfb257bf8266
Milind Barve <milind@datatorrent.com>
2013-12-11 19:17:29 +0530

    Added licensing information and comments.

--- library/src/main/java/com/datatorrent/lib/r/RMin.java
+++ library/src/main/java/com/datatorrent/lib/r/RMin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.datatorrent.lib.r;
 
 import com.datatorrent.api.*;
@@ -13,28 +29,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Emits at end of window, a minimum of all values sub-classed from Number in the incoming stream. <br>
+ * Emits at the end of window, a minimum of all values sub-classed from Number in the incoming stream. <br>
+ * A vector is created in the process method. This vector is passed to the 'min' function in R in the
+ * 'endWindow' function.
  * <br>
- * <b>StateFull :</b>Yes, min value is computed over application windows. <br>
- * <b>Partitions :</b>Yes, operator is kin unifier operator. , <br>
+ * <b>StateFull :</b>Yes, max value is computed over application windows. <br>
+ * <b>Partitions :</b>Yes, operator is kin unifier operator. <br>
  * <br>
  * <b>Ports</b>:<br>
  * <b>data</b>: expects V extends Number<br>
- * <b>min</b>: emits V extends Number<br>
+ * <b>max</b>: emits V extends Number<br>
  * <br>
  * <br>
  **/
 
-//public class RMin<V extends Number> extends BaseNumberValueOperator implements Operator.Unifier<V>
 public class RMin<V extends Number> extends BaseNumberValueOperator<Number> implements Unifier<Number>
 {
     private List<Number> vector = new ArrayList<Number>();
     private Rengine rengine;
 
     private static Logger log = LoggerFactory.getLogger(RMin.class);
-
-    //@InputPortFieldAnnotation(name = "data")
-    //public final transient DefaultInputPort<Number> data = new DefaultInputPort<Number>()
 
     @InputPortFieldAnnotation(name = "data")
 
@@ -87,10 +101,9 @@ public class RMin<V extends Number> extends BaseNumberValueOperator<Number> impl
     }
 
     /**
-     * Emits a minimum from the values received in teh application window.
+     * Invokes the 'min' function from R to emit a maximum from the values received in the application window.
      * Clears the vector at the end of the function.
      **/
-
     @Override
     public void endWindow() {
 
