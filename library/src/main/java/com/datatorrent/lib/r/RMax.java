2de015f5967ce88309e21d2b85864669e6244fdf
Milind Barve <milind@datatorrent.com>
2013-12-11 19:16:50 +0530

    Added licensing information and comments.

--- library/src/main/java/com/datatorrent/lib/r/RMax.java
+++ library/src/main/java/com/datatorrent/lib/r/RMax.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.datatorrent.lib.r;
 
 import com.datatorrent.api.Context;
@@ -17,6 +33,8 @@ import java.util.List;
 
 /**
  * Emits at end of window, a maximum of all values sub-classed from Number in the incoming stream. <br>
+ * A vector is created in the process method. This vector is passed to the 'max' operator in R in the
+ * 'endWindow' function.
  * <br>
  * <b>StateFull :</b>Yes, max value is computed over application windows. <br>
  * <b>Partitions :</b>Yes, operator is kin unifier operator. <br>
@@ -28,8 +46,6 @@ import java.util.List;
  * <br>
  **/
 
-
-// public class RMax<V extends Number> extends BaseNumberValueOperator {
 public class RMax<V extends Number> extends BaseNumberValueOperator<Number> implements Unifier<Number>{
     private List<Number> vector = new ArrayList<Number>();
     private Rengine rengine;
@@ -85,7 +101,7 @@ public class RMax<V extends Number> extends BaseNumberValueOperator<Number> impl
     }
 
     /**
-     * Emits a maximum from the values received in the application window.
+     * Invokes the 'max' function from R to emit a maximum from the values received in the application window.
      * Clears the vector at the end of the function.
      **/
 
