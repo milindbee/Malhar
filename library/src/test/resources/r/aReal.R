1b5d5789e7052913b12a20336a7a2f7261bc5487
Milind Barve <milind@datatorrent.com>
2013-12-30 23:30:19 +0530

    R script for testing real datatypes changed. Added coments as well. #302

--- library/src/test/resources/r/aReal.R
+++ library/src/test/resources/r/aReal.R
@@ -1,8 +1,10 @@
 #!/usr/bin/Rscript
 
 # This script takes two real numbers and returns their sum as a double.
+#
 # Used to test the "real' data type(s) being passed from and to the 
 # Rscript operator for Malhar.
+#
 # num1 and num2 are passed as arguments by the client and retVal is the 
 # result returned by the script.
 
